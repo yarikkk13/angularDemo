666ade09c8de9d0c090e1e8bc80790dbac4dd9a8
yarikkk13 <yaroslav.mahas@gmail.com>
2021-05-24 16:21:05 +0300

    video 06 complete,    in Post.ts export interface - what is it?,    post.component.ts - elvis 11line,    don't forget about selector in app.component.ts,

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Post} from 'src/app/models/Post';
+import {PostService} from "../../servisces/post/post.service";
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export class PostComponent implements OnInit {
+export class PostComponent {
+  posts?: Post[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private postService: PostService) {
+    postService.getAllPosts().subscribe(value => this.posts = value)
   }
+
 
 }
